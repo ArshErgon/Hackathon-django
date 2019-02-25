229ead0bb46d05c8e8dfb1f9b301eef69583e863
ArshErgon <40994679+ArshErgon@users.noreply.github.com>
2019-02-25 21:50:28 +0530

    Update forms.py

--- publicpeople/forms.py
+++ publicpeople/forms.py
@@ -6,44 +6,44 @@ class PublicForm(forms.Form):
 		('100','100'),
 		)
 	name = forms.CharField(
-						widget=forms.TextInput(
-											attrs={
-													'class':'form-control'
-													}
-												)
+			widget=forms.TextInput(
+						attrs={
+							'class':'form-control'
+							}
 						)
+				)
 
 	father_name = forms.CharField(
-							widget=forms.TextInput(
-													attrs={
-															'class':'form-control'
-															}
-													)
+				widget=forms.TextInput(
+							attrs={
+								'class':'form-control'
+								}
 							)
+					)
 
 	mother_name = forms.CharField(
-							widget=forms.TextInput(
-													attrs={
-															'class':'form-control'
-															}
-													)
+				widget=forms.TextInput(
+							attrs={
+								'class':'form-control'
+								}
 							)
+				)
 
 	email = forms.CharField(
-							widget=forms.TextInput(
-													attrs={
-															'class':'form-control'
-															}
-													)
+				widget=forms.TextInput(
+							attrs={
+								'class':'form-control'
+								}
 							)
+				)
 
 	phone_number = forms.CharField(
-								widget=forms.NumberInput(
-														attrs={
-																"class":'form-control'
-																}
-														)
+					widget=forms.NumberInput(
+								attrs={
+									"class":'form-control'
+									}
 								)
+					)
 	
 
 	def clean_phone_number(self):
