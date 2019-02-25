9b314d0cfc024ce2d5106a6108979a357774e860
ArshErgon <40994679+ArshErgon@users.noreply.github.com>
2019-02-25 21:38:31 +0530

    Update models.py

--- publicpeople/models.py
+++ publicpeople/models.py
@@ -5,7 +5,7 @@ class PublicRegistration(models.Model):
 	person_father_name 			=		 models.CharField(max_length=100)
 	person_mother_name 			=		 models.CharField(max_length=100)
 	person_email 				=		 models.EmailField()
-	person_phone_number 		=		 models.IntegerField()
+	person_phone_number 			=		 models.IntegerField()
 	
 
 	def __str__(self):
